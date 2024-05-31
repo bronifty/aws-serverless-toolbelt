92eef1f6b4c51691ebe8e8c6d3b9e11b06d5ec85
bronifty <bronifty@gmail.com>
2024-05-30 21:50:45 -0400

    this

--- scripts/function/lambda_deploy.sh
+++ scripts/function/lambda_deploy.sh
@@ -8,8 +8,8 @@
 
    # Check if a function name was passed as an argument
    if [ -z "$1" ]; then
-       echo "No function name provided. Using default function name."
        FUNCTION_NAME=$DEFAULT_FUNCTION_NAME
+       echo "No function name provided. Using default function name: $FUNCTION_NAME"
    else
        FUNCTION_NAME=$1  # Set the function name from the first script argument
    fi
@@ -19,5 +19,4 @@
        --runtime $LAMBDA_RUNTIME \
        --role $LAMBDA_ROLE \
        --handler $LAMBDA_HANDLER \
-       --zip-file fileb://$FUNCTION_NAME.zip \
-       --layers ""
+       --zip-file fileb://$FUNCTION_NAME.zip 
--- scripts/function/lambda_invoke.sh
+++ scripts/function/lambda_invoke.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check if a function name was passed as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <function-name>"
-    exit 1
-fi
-
-FUNCTION_NAME=$1  # Set the function name from the first script argument
+   if [ -z "$1" ]; then
+       echo "No function name provided. Using default function name."
+       FUNCTION_NAME=$DEFAULT_FUNCTION_NAME
+   else
+       FUNCTION_NAME=$1  # Set the function name from the first script argument
+   fi
 
 aws lambda invoke \
     --function-name $FUNCTION_NAME \
