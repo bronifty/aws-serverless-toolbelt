21cd38b14d35069379dc0165f463520064624113
bronifty <bronifty@gmail.com>
2024-06-01 13:38:09 -0400

    this

--- scripts/function/function-get-one.sh
+++ scripts/function/function-get-one.sh
@@ -25,7 +25,7 @@ source $SCRIPT_DIR/../variables.sh
 
 # Check if a function name was passed as an argument
 if [ -z "$1" ]; then
-    echo "No function name provided. Using default function name."
+    echo "No function name provided. Using default function name: $DEFAULT_FUNCTION_NAME"
     FUNCTION_NAME=$DEFAULT_FUNCTION_NAME
 else
     FUNCTION_NAME=$1  # Set the function name from the first script argument
@@ -40,3 +40,43 @@ if [ -z "$FUNCTION_ARN" ]; then
 else
     aws lambda get-function-configuration --function-name $FUNCTION_ARN
 fi
+
+
+# aws lambda list-functions
+# {
+#   "Functions": [
+#     {
+#       "FunctionName": "function",
+#       "FunctionArn": "arn:aws:lambda:us-east-1:851725517932:function:function",
+#       "Runtime": "nodejs20.x",
+#       "Role": "arn:aws:iam::851725517932:role/lambda-full-access",
+#       "Handler": "index.handler",
+#       "CodeSize": 3327808,
+#       "Description": "",
+#       "Timeout": 3,
+#       "MemorySize": 128,
+#       "LastModified": "2024-06-01T03:58:53.971+0000",
+#       "CodeSha256": "tulU60LvHUsDyid6TjB1Q8rg9lR+b9YMVgMgllCv7EI=",
+#       "Version": "$LATEST",
+#       "TracingConfig": {
+#         "Mode": "PassThrough"
+#       },
+#       "RevisionId": "5c44ac30-8ccb-4a7e-81ba-9b4f8cc8cf26",
+#       "PackageType": "Zip",
+#       "Architectures": [
+#         "x86_64"
+#       ],
+#       "EphemeralStorage": {
+#         "Size": 512
+#       },
+#       "SnapStart": {
+#         "ApplyOn": "None",
+#         "OptimizationStatus": "Off"
+#       },
+#       "LoggingConfig": {
+#         "LogFormat": "Text",
+#         "LogGroup": "/aws/lambda/function"
+#       }
+#     }
+#   ]
+# }
