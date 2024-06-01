c63df86f30c73048d88054a24d6a9aba1d413049
bronifty <bronifty@gmail.com>
2024-05-31 23:45:28 -0400

    this

--- scripts/cloud-admin/bootstrap.sh
+++ scripts/cloud-admin/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# cdk bootstrap aws://851725517932/us-east-1
 # Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
@@ -8,3 +9,5 @@ source $SCRIPT_DIR/../variables.sh
 account_id=$(aws sts get-caller-identity --query "Account" --output text)
 region=$(aws configure get region)
 ./scripts/cloud-admin/cdk.sh $account_id $region
+
+
